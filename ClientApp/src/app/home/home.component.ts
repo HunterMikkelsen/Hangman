97fbebf7219e7413a48b762d8eb79084d5cc872f
Scuvy <59233814+Scuvy@users.noreply.github.com>
2022-02-17 19:02:33 -0700

    Initial commit
    page logic broken...Does look pretty tho

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -11,7 +11,7 @@ import { HighScore } from '../angular-models/highscore.model';
 export class HomeComponent {
   word: string;
   wordLengthArray: string[];
-  letters: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  letters: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   correctlyGuessedLetters: string[] = [];
   incorrectlyGuessedLetters: string[] = [];
   pictureUrl: string = "/assets/images/hangman1.png";
